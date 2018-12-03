8241055c327db6981a6b48dfdd445a5c9eca48cd
Rijad <rsehovic1@etf.unsa.ba>
2018-12-03 12:31:36 +0100

    Initial commit

---
+++ src/ba/unsa/rpr/tutorijal7/UN.java
@@ -0,0 +1,18 @@
+package ba.unsa.rpr.tutorijal7;
+
+import java.util.ArrayList;
+
+public class UN {
+    ArrayList<Drzava> drzave;
+
+    public UN() {
+    }
+
+    public ArrayList<Drzava> getDrzave() {
+        return drzave;
+    }
+
+    public void setDrzave(ArrayList<Drzava> drzave) {
+        this.drzave = drzave;
+    }
+}
